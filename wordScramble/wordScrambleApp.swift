cedecf791328bae97121b0afba8851b5f39f1023
Nadia <nadia.ahmadian96@yahoo.com>
2023-01-15 18:28:24 +0330

    Initial Commit

---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Nadia Ahmadian on 2023-01-15.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
